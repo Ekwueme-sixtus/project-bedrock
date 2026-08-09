c02dc8943337d5a5ac6e343ceb4ed5446c6d4313
Ekwueme Valerian <abuchi40@gmail.com>
2026-08-09 03:45:39 +0000

    Add VPC module (project-bedrock-vpc) - applied successfully

---
+++ terraform/main.tf
@@ -0,0 +1,57 @@
+terraform {
+  required_version = ">= 1.11.0"
+
+  backend "s3" {
+    bucket       = "bedrock-tf-state-alt-soe-tin-025-0080"
+    key          = "project-bedrock/terraform.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
+    encrypt      = true
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+
+  default_tags {
+    tags = {
+      Project = "tinyuka-2025-capstone"
+    }
+  }
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
+
+  name = "project-bedrock-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = "1"
+  }
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = "1"
+  }
+
+  tags = {
+    Project = "tinyuka-2025-capstone"
+  }
+}
