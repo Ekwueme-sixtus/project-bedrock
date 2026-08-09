3abeb2e32c7d6f09766ea34102fa201c7ae7face
Ekwueme Valerian <abuchi40@gmail.com>
2026-08-09 00:34:42 +0000

    Bootstrap TF state backend - S3 bucket created

---
+++ terraform/bootstrap/main.tf
@@ -0,0 +1,49 @@
+terraform {
+  required_version = ">= 1.11.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "tf_state" {
+  bucket = "bedrock-tf-state-${var.student_id}"
+
+  tags = {
+    Project = "tinyuka-2025-capstone"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "tf_state" {
+  bucket = aws_s3_bucket.tf_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "tf_state" {
+  bucket = aws_s3_bucket.tf_state.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "tf_state" {
+  bucket                  = aws_s3_bucket.tf_state.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+variable "student_id" {
+  type = string
+}
