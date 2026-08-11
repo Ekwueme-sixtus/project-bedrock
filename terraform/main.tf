6d7ef19361293f616cdf8d83d9f84c735641ea08
Ekwueme Valerian <abuchi40@gmail.com>
2026-08-11 02:06:54 +0000

    Fix GitHub OIDC trust policy to account for immutable ID format in sub claim

--- terraform/main.tf
+++ terraform/main.tf
@@ -506,7 +506,7 @@ data "aws_iam_policy_document" "github_actions_assume" {
     condition {
       test     = "StringLike"
       variable = "token.actions.githubusercontent.com:sub"
-      values   = ["repo:Ekwueme-sixtus/project-bedrock:*"]
+      values   = ["repo:Ekwueme-sixtus*/project-bedrock*:*"]
     }
   }
 }
